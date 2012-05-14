797609d7204728568271a0e74a58d6d867ccd615
Carles Barrobés <carles.barrobes@glassesdirect.com>
2012-05-14 17:40:51 +0100

    Version with http.server and bottle

--- pretenders/http/server.py
+++ pretenders/http/server.py
@@ -1,41 +1,74 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
 
 
-next_response = 'Response'.encode('utf-8')
+next_responses = []
 
 
 class ReplayRequestHandler(BaseHTTPRequestHandler):
 
     def __getattr__(self, attr):
-        if attr.startswith('do_'):
+        if attr.startswith("do_"):
             print("Getting attr {0}".format(attr))
             return self.dispatch
         else:
             raise AttributeError("'{}' not found".format(attr))
 
     def dispatch(self):
-        global next_response
         data = None
         print("{0} {1}".format(self.command, self.path))
         print("Headers:\n{0}".format(self.headers))
-        length = int(self.headers.get('Content-Length', 0))
+        length = int(self.headers.get("Content-Length", 0))
         if length:
             data = self.rfile.read(length)
             print("Data:\n{0}".format(data))
-            if self.command == 'RECORD':
-                next_response = data
-        self.send_response(200)
-        self.send_header('Content-Length', len(next_response))
+        if self.command == "RECORD":
+            next_responses.append(data)
+            self.respond(200, b"OK")
+        elif len(next_responses):
+            body = next_responses.pop(0)
+            self.respond(200, body)
+        else:
+            self.respond(404, b"No preset response")
+
+    def respond(self, status, body):
+        self.send_response(status)
+        self.send_header("Content-Length", len(body))
         self.end_headers()
-        self.wfile.write(next_response)
-
-    #do_GET = dispatch
-    #do_POST = dispatch
-    #do_HEAD = dispatch
-    #do_OPTIONS = dispatch
+        self.wfile.write(body)
 
 
-if __name__ == '__main__':
-        server_address = ('', 8000)
-        httpd = HTTPServer(server_address, ReplayRequestHandler)
-        httpd.serve_forever()
+def run_raw(port=8000):
+    server_address = ("localhost", port)
+    httpd = HTTPServer(server_address, ReplayRequestHandler)
+    httpd.serve_forever()
+
+
+from bottle import request, response, route, run, get, post
+
+
+def respond(status, body):
+    response.status = status
+    return body
+
+
+@route('/mock', method='ANY')
+def replay():
+    if len(next_responses):
+        body = next_responses.pop(0)
+        return respond(200, body)
+    else:
+        return respond(404, b"No preset response")
+
+
+@post('/preset')
+def add_preset():
+    next_responses.append(request.body)
+    return respond(200, b"OK")
+
+
+def run_bottle(port=8000):
+    run(host='localhost', port=port)
+
+
+if __name__ == "__main__":
+    run_bottle()
