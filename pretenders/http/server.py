d75b59e7ef1cac73ff418a31b2ccdce8fb2a2009
Carles Barrobés <carles.barrobes@glassesdirect.com>
2012-05-15 16:00:14 +0100

    Use proprietary headers for additional data

--- pretenders/http/server.py
+++ pretenders/http/server.py
@@ -1,6 +1,4 @@
-import json
-
-from bottle import request, route, run, HTTPResponse
+from bottle import request, response, route, run, HTTPResponse
 from bottle import delete, get, post
 
 
@@ -15,23 +13,25 @@ def to_dict(wsgi_headers):
     return {k: v for k, v in wsgi_headers.items()}
 
 
-@route('/mock', method='ANY')
-def replay():
+@route('/mock<path:path>', method='ANY')
+def replay(path):
     """
     Replay a previously recorded preset, and save the request in history
     """
     if not len(presets):
         raise HTTPResponse(b"No preset response", status=404)
-    encoding = 'utf-8'  # find out proper value?
     saved_request = {
-        'body': request.body.read().decode(encoding),
+        'body': request.body.read(),
         'headers': to_dict(request.headers),
         'method': request.method,
-        'path': request.path,
+        'path': path,
     }
     history.append(saved_request)
-    body = presets.pop(0)
-    return body
+    preset = presets.pop(0)
+    for header, value in preset['headers'].items():
+        response.set_header(header, value)
+    response.status = preset['status']
+    return preset['body']
 
 
 @post('/preset')
@@ -39,8 +39,15 @@ def add_preset():
     """
     Save the incoming request body as a preset response
     """
-    # TODO: JSON decode in body, status, headers...
-    presets.append(request.body)
+    headers = {
+        k: v for k, v in request.headers.items()
+        if not k.startswith('X-Pretend-')
+    }
+    presets.append({
+        'body': request.body,
+        'status': int(request.headers.get('X-Pretend-Response-Status', 200)),
+        'headers': headers,
+    })
 
 
 @delete('/preset')
@@ -55,7 +62,11 @@ def clear_presets():
 def get_history(ordinal):
     try:
         saved_request = history[ordinal]
-        return json.dumps(saved_request)
+        for header, value in saved_request['headers'].items():
+            response.set_header(header, value)
+        response.set_header('X-Pretend-Request-Method', saved_request['method'])
+        response.set_header('X-Pretend-Request-Path', saved_request['path'])
+        return saved_request['body']
     except IndexError:
         raise HTTPResponse(b"No recorded request", status=404)
 
