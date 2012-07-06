1c9a163eb77a49b3dc7192df41db48f5036ab497
Alex Couper <alex.couper@glassesdirect.com>
2012-07-06 17:21:05 +0100

    increase commit count

--- pretenders/boss/client.py
+++ pretenders/boss/client.py
@@ -48,7 +48,6 @@ class BossClient(object):
                                              url=self.create_mock_url,
                                              body=post_body)
             mock_server_json = response.read().decode('ascii')
-            print(mock_server_json)
             mock_server_details = json.loads(mock_server_json)
             return mock_server_details["url"], mock_server_details["id"]
         return "", ""
