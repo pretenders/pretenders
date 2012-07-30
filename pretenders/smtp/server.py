3457968e64d9bfae135fdc4c7472fd3fe4312e9b
Alex Couper <alex.couper@glassesdirect.com>
2012-07-30 18:16:28 +0100

    Get SMTP tests running

--- pretenders/smtp/server.py
+++ pretenders/smtp/server.py
@@ -16,10 +16,6 @@ class MockSMTPServer(smtpd.SMTPServer, Pretender):
         LOGGER.info("Initialised MockSMTPServer")
 
     def process_message(self, peer, mailfrom, rcpttos, data):
-        print('Receiving message from:', peer)
-        print('Message addressed from:', mailfrom)
-        print('Message addressed to  :', rcpttos)
-        print('Message length        :', len(data))
         smtp_info = SMTPSerialiser(
             peer=peer, mailfrom=mailfrom, rcpttos=rcpttos, data=data,
             match='')
