a41675de7746bba60bb1346e579296e909d82e6a
Carles Barrobés <carles.barrobes@glassesdirect.com>
2012-07-30 22:28:44 +0100

    Add running smtp tests to build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
-nosetests pretenders/http/tests
+nosetests pretenders/http/tests pretenders/smtp/tests
 pep8 pretenders > pep8.txt || echo "PEP8 errors"
 (cd docs; make clean html)
