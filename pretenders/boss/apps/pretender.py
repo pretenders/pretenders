7a3f5b3c5716b93d604bd4dcbbf23993f8941d6d
Carles <carles@barrobes.com>
2012-10-21 14:16:29 +0100

    Move protocol-specific handlers to their own packages

--- pretenders/boss/apps/pretender.py
+++ pretenders/boss/apps/pretender.py
@@ -6,117 +6,12 @@ from bottle import delete, get, post, HTTPResponse
 
 from pretenders import settings
 from pretenders.base import get_logger
+from pretenders.http.handler import HttpHandler
+from pretenders.smtp.handler import SmtpHandler
 
 
 LOGGER = get_logger('pretenders.boss.apps.pretender')
 UID_COUNTER = 0
-"Dictionary containing details of currently active pretenders"
-PRETENDERS = {
-    'http': {},
-    'smtp': {},
-}
-
-
-from pretenders.boss import HTTPPretenderModel
-
-
-class HttpHandler(object):
-
-    def new_pretender(self, uid, timeout):
-        start = datetime.datetime.now()
-
-        PRETENDERS['http'][uid] = HTTPPretenderModel(
-            start=start,
-            timeout=datetime.timedelta(seconds=timeout),
-            last_call=start,
-            uid=uid,
-        )
-
-        return json.dumps({
-            'path': "/mockhttp/{0}".format(uid),
-            'id': uid})
-
-    def delete_pretender(self, uid):
-        "Delete a pretender by ``uid``"
-        del PRETENDERS['http'][uid]
-
-
-import os
-import signal
-import subprocess
-import sys
-import time
-from pretenders.boss import SMTPPretenderModel
-from pretenders.constants import (
-    RETURN_CODE_PORT_IN_USE,
-    PRETEND_PORT_RANGE
-)
-from pretenders.boss import data
-from pretenders.exceptions import NoPortAvailableException
-
-
-class SmtpHandler(object):
-
-    def available_ports(self):
-        """
-        Get a set of ports available for starting pretenders
-        """
-        ports_in_use = set(map(lambda x: x.port, PRETENDERS['smtp'].values()))
-        available_set = PRETEND_PORT_RANGE.difference(ports_in_use)
-        return available_set
-
-    def new_pretender(self, uid, timeout):
-        """
-        Launch a new SMTP pretender in a separate process.
-
-        It will first look for an available port.
-        """
-        for port_number in self.available_ports():
-            LOGGER.info(
-                "Attempt to start smtp pretender on port {0}, timeout {1}"
-                .format(port_number, timeout))
-            process = subprocess.Popen([
-                sys.executable,
-                "-m",
-                "pretenders.smtp.server",
-                "-H", "localhost",
-                "-p", str(port_number),
-                "-b", str(data.BOSS_PORT),
-                "-i", str(uid),
-                ])
-            time.sleep(2)  # Wait this long for failure
-            process.poll()
-            if process.returncode == RETURN_CODE_PORT_IN_USE:
-                LOGGER.info("Return code already set. "
-                            "Assuming failed due to socket error.")
-                continue
-            start = datetime.datetime.now()
-            PRETENDERS['smtp'][uid] = SMTPPretenderModel(
-                start=start,
-                port=port_number,
-                pid=process.pid,
-                timeout=datetime.timedelta(seconds=timeout),
-                last_call=start,
-                uid=uid,
-            )
-            LOGGER.info("Started smtp pretender on port {0}".format(
-                port_number))
-            return json.dumps({
-                'full_host': "localhost:{0}".format(port_number),
-                'id': uid})
-        raise NoPortAvailableException("All ports in range in use")
-
-    def delete_pretender(self, uid):
-        "Delete a pretender by ``uid``"
-        LOGGER.info("Performing delete on {0}".format(uid))
-        pid = PRETENDERS['smtp'][uid].pid
-        LOGGER.info("attempting to kill pid {0}".format(pid))
-        try:
-            os.kill(pid, signal.SIGKILL)
-            del PRETENDERS['smtp'][uid]
-        except OSError as e:
-            LOGGER.info("OSError while killing:\n{0}".format(dir(e)))
-
 
 HANDLERS = {
     'http': HttpHandler(),
@@ -124,18 +19,28 @@ HANDLERS = {
 }
 
 
+def get_pretenders(protocol):
+    """
+    Get a dict mapping UID to pretender data for the given protocol
+    """
+    return HANDLERS[protocol].PRETENDERS
+
+
 def keep_alive(uid, protocol):
     """
     Notification from a mock server that it must be kept  alive.
     """
-    PRETENDERS[protocol][uid].keep_alive()
+    get_pretenders(protocol)[uid].keep_alive()
 
 
 @get('/<protocol:re:(http|smtp)>/<uid:int>')
 def pretender_get(protocol, uid):
+    """
+    Get details for a given pretender, defined by protocol and UID
+    """
     bottle.response.content_type = 'application/json'
     try:
-        return PRETENDERS[protocol][uid].as_json()
+        return get_pretenders(protocol)[uid].as_json()
     except KeyError:
         raise HTTPResponse(b"No matching {0} mock".format(protocol),
                            status=404)
@@ -189,7 +94,7 @@ def pretender_delete(protocol):
         LOGGER.debug("Got request to delete stale pretenders")
         # Delete all stale requests
         now = datetime.datetime.now()
-        for uid, server in PRETENDERS[protocol].copy().items():
+        for uid, server in get_pretenders(protocol).copy().items():
             LOGGER.debug("Pretender: {0}".format(server))
             if server.last_call + server.timeout < now:
                 LOGGER.info("Deleting pretender with UID: {0}".format(uid))
--- pretenders/smtp/tests/unit/test_smtp_handler.py
+++ pretenders/smtp/tests/unit/test_smtp_handler.py
@@ -3,8 +3,7 @@ from nose.tools import (
 from mock import Mock
 
 from pretenders.constants import PRETEND_PORT_RANGE
-from pretenders.boss.apps.pretender_smtp import (available_ports,
-                                                 SMTP_PRETENDERS)
+from pretenders.smtp.handler import SmtpHandler
 
 
 def test_available_ports():
@@ -12,7 +11,8 @@ def test_available_ports():
     Test ``available_ports`` filters out ports that are in use by
     ``MOCK_SERVERS``.
     """
-    ports = available_ports()
+    handler = SmtpHandler()
+    ports = handler.available_ports()
 
     assert_equals(ports, PRETEND_PORT_RANGE)
 
@@ -22,10 +22,10 @@ def test_available_ports():
 
     pretend_server = Mock()
     pretend_server.port = mock_port
-    SMTP_PRETENDERS[1] = pretend_server
+    handler.PRETENDERS[1] = pretend_server
 
     # Now they should not be equal
-    ports = available_ports()
+    ports = handler.available_ports()
 
     assert_not_equals(ports, PRETEND_PORT_RANGE)
 
