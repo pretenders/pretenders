72c3585b5abb7a44d96bb570839cbbca88aa715c
Alex Couper <alex.couper@glassesdirect.com>
2013-03-14 16:31:03 +0000

    Add logging middleware.

--- pretenders/server/__init__.py
+++ pretenders/server/__init__.py
@@ -1,7 +1,10 @@
 import bottle
+from .middleware import ExceptionLoggingMiddleware
 
 app = bottle.Bottle()
+app.catchall = False
 
+myapp = ExceptionLoggingMiddleware(app)
 
 # Import apps so that they get initialised for bottle.
 from pretenders.server.apps import history, preset, replay, pretender  # NOQA
