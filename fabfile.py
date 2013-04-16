3b7301375ff612ac0e5ea4f4979f425162b33e70
Carles Barrobés <carles@barrobes.com>
2013-04-16 08:49:16 +0100

    Clean up fabfile

--- fabfile.py
+++ fabfile.py
@@ -2,18 +2,18 @@ from fabric.api import task, put, run, get, local
 from fabric.contrib.project import rsync_project
 
 
-#DAEMON = '~/init/wishlist'
 LIGHTTPD = '~/init/lighttpd'
 LIGHTTPD_CONF = '~/lighttpd/pretenders.conf'
 SRV_HOME = '~/srv/pretenders'
 ENV_HOME = '~/env/pretenders'
 PYTHON = ENV_HOME + '/bin/python'
-# MANAGE = '{0} {1}'.format(PYTHON, SRV_HOME + '/manage.py')
 
 
 @task
 def deploy():
-    """Copy project to txels.com"""
+    """
+    Deploy to remote server and restart.
+    """
     rsync_project(
         local_dir='.',
         remote_dir=SRV_HOME,
